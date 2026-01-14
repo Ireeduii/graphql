3f0cd8d042c92725ccca45512b75502f2c8f9f74
25LP4866@nest.edu.mn <25lp4866nest.edu.mn@G301-CC0211.local>
2026-01-14 16:18:14 +0800

    layout done

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -1,5 +1,16 @@
 "use client"
 
+import { useQuery } from "@apollo/client/react"
+import {gql} from '@apollo/client'
+
 export default function Page() {
+    const {loading, error, data} = useQuery(gql `
+        query  {
+        hello
+        getArticles {
+           title}}
+        `)
+
+        console.log({loading, error, data})
     return <div>GraphQL</div>
 }
